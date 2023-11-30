ce3162bc3eea43b5a0bcbfc80307687f03a6e5db
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-11-30 09:25:31 +0530

    Hcm mdms v2 (#274)
    * Initial commit mdms-v2
    
    * Added custom validator for json schema
    
    * [MDMS-V2] Refactored code and added composite id generation and reference resolver methods
    
    * [DPG-1503] Making changes to deploy mdms v2 as a separate service
    
    * [DPG-1503] Removed redis related beans
    
    * [DPG-1503] Changed servelet context path to mdms-v2
    
    * [DPG-1503] Added management API endpoints
    
    * [DPG-1503] Removed unwanted bean creation for redis
    
    * [DPG-1503] Corrected validation for x-ref-schema
    
    * [DPG-1683] Refactored code and moved common methods to utility classes
    
    * [1683] Refactored and added comments
    
    * [DPG-1683] Pushing code refactor changes
    
    * [DPG-1683] Adding search on schemaCode
    
    * [DPG-1683] Refactored code and added comments
    
    * [DPG-1683] Refactoring and adding comments
    
    * [DPG-1683] Adding code for master data update API
    
    * [DPG-1683] Correcting MDMS v2 update flow
    
    * [DPG-1683] Moving update mdms data topic to application.properties
    
    * [DPG-1709] Making schema code non mandatory in schema search criteria
    
    * [DPG-1684] Adding fallback logic in both v1 and v2 search APIs
    
    * [DPG-1683] Removing unsused methods
    
    * [DPG-1683] corrected build path for mdms-v2
    
    * [DPG-1711] Modified code to accept reference unique identifier rather than id while creating referenced master data
    
    * [DPG-1685] Adding validations to schema creation flow
    
    * [DPG-1685] Added support for passing list of schema codes in v2 data search and removed schema code from context path
    
    * [DPG-1685] Adding comments
    
    * [DPG-1685] Adding pagination support to schema search and data v2 search
    
    * [DPG-1685] Adding support for nested path and array metrics in ref resolver
    
    * [DPG-1777] Adding isActive filter in v2 search, functionality to show only active master data by default in v1 search
    
    * [DPG-1779] Fixed bug to allow creation of master data with the same unique identifier across schemas
    
    * [DPG-1793] Added functionality to make fallback logic work across schema codes
    
    * Changed parallelStream to stream in fallback logic
    
    * Corrected fallback logic
    
    * MDMS: Made changes to fallback and search
    
    * Updated validator
    
    * fixed reference logic accross schema codes
    
    * [DPG-1899] Corrected ref validation logic
    
    * [DPG-1899] Refactored validation code
    
    * [MDMS-RELEASE] Changed uniqueIdentifier to list
    
    * [MDMS-RELEASE] Fixed filter in v1 search API
    
    * Adding isActive as criteria for validating master data creation
    
    * Changed mdmsresponse name to MdmsResponse
    
    * [DPG-1995] Change the min size of uniqueIdentifier from 2 to 1
    
    * [DPG-1996] Added fallback in reference validation
    
    * Update build-config.yml
    
    ---------
    
    Co-authored-by: Ghanshyam <ghanshyam@egovs-MacBook-Pro.local>
    Co-authored-by: Shashwat Mishra <shashwat.mishra@egovernments.org>
    Co-authored-by: Shailesh Pandey <110380977+shailesh-egov@users.noreply.github.com>
    Co-authored-by: shubhang-egov <shubhang.dhanesha@egovernments.org>
    Co-authored-by: Rishabh-egov <rishabh.jain@egovernments.org>

--- core-services/egov-mdms-service/src/main/java/org/egov/infra/mdms/service/MDMSService.java
+++ core-services/egov-mdms-service/src/main/java/org/egov/infra/mdms/service/MDMSService.java
@@ -1,197 +1,150 @@
 package org.egov.infra.mdms.service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-import org.egov.MDMSApplicationRunnerImpl;
-import org.egov.mdms.model.MasterDetail;
-import org.egov.mdms.model.MdmsCriteriaReq;
-import org.egov.mdms.model.ModuleDetail;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.jayway.jsonpath.JsonPath;
+import org.egov.common.utils.MultiStateInstanceUtil;
+import org.egov.infra.mdms.model.*;
+import org.egov.infra.mdms.repository.MdmsDataRepository;
+import org.egov.infra.mdms.service.enrichment.MdmsDataEnricher;
+import org.egov.infra.mdms.service.validator.MdmsDataValidator;
+import org.egov.infra.mdms.utils.FallbackUtil;
+import org.egov.infra.mdms.utils.SchemaUtil;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 import net.minidev.json.JSONArray;
+import org.springframework.util.ObjectUtils;
+
+import static org.egov.infra.mdms.utils.MDMSConstants.*;
 
 @Service
 @Slf4j
 public class MDMSService {
 
+	private MdmsDataValidator mdmsDataValidator;
+
+	private MdmsDataEnricher mdmsDataEnricher;
+
+	private MdmsDataRepository mdmsDataRepository;
+
+	private SchemaUtil schemaUtil;
+
+	private MultiStateInstanceUtil multiStateInstanceUtil;
+
+	@Autowired
+	public MDMSService(MdmsDataValidator mdmsDataValidator, MdmsDataEnricher mdmsDataEnricher,
+					   MdmsDataRepository mdmsDataRepository, SchemaUtil schemaUtil, MultiStateInstanceUtil multiStateInstanceUtil) {
+		this.mdmsDataValidator = mdmsDataValidator;
+		this.mdmsDataEnricher = mdmsDataEnricher;
+		this.mdmsDataRepository = mdmsDataRepository;
+		this.schemaUtil = schemaUtil;
+		this.multiStateInstanceUtil = multiStateInstanceUtil;
+	}
+
 	/**
-	 * Service method to collect master data from tenantIdMap and apply filter as per the request
-	 * 
-	 * @param mdmsCriteriaReq
-	 * @return Map<String, Map<String, JSONArray>> masterData
+	 * This method processes the requests that come for master data creation.
+	 * @param mdmsRequest
+	 * @return
 	 */
-	public Map<String, Map<String, JSONArray>> searchMaster(MdmsCriteriaReq mdmsCriteriaReq) {
+	public List<Mdms> create(MdmsRequest mdmsRequest) {
 
-		Map<String, Map<String, Map<String, JSONArray>>> tenantIdMap = MDMSApplicationRunnerImpl.getTenantMap();
+		// Fetch schema against which data is getting created
+		JSONObject schemaObject = schemaUtil.getSchema(mdmsRequest);
 
-		String tenantId = mdmsCriteriaReq.getMdmsCriteria().getTenantId();
-		log.info(" Incoming tenantid : " + tenantId);
-		
-		/* 
-		 * local tenantId replica for backtracking to parent tenant when child tenant is empty
-		 */
-		String tenantIdWithData = tenantId;
+		// Validate incoming request
+		mdmsDataValidator.validateCreateRequest(mdmsRequest, schemaObject);
 
-		int countOfSubTenant = StringUtils.countOccurrencesOf(tenantId, ".");
-		Map<String, Map<String, JSONArray>> tenantData = tenantIdMap.get(tenantId);
-		Map<String, Map<String, JSONArray>> responseMap = new HashMap<>();
+		// Enrich incoming request
+		mdmsDataEnricher.enrichCreateRequest(mdmsRequest, schemaObject);
+
+		// Emit mdms creation request event
+		mdmsDataRepository.create(mdmsRequest);
+
+		return Arrays.asList(mdmsRequest.getMdms());
+	}
+
+	/**
+	 * This method processes the requests that come for master data search.
+	 * @param mdmsCriteriaReq
+	 * @return
+	 */
+	public Map<String, Map<String, JSONArray>> search(MdmsCriteriaReq mdmsCriteriaReq) {
+		Map<String, Map<String, JSONArray>> tenantMasterMap = new HashMap<>();
 
 		/*
-		 * if the tenantId doesn't contain a separator
+		 * Set incoming tenantId as state level tenantId for fallback in case master data for
+		 * concrete tenantId does not exist.
 		 */
-		if (countOfSubTenant == 0) {
+		String tenantId = new StringBuilder(mdmsCriteriaReq.getMdmsCriteria().getTenantId()).toString();
+		mdmsCriteriaReq.getMdmsCriteria().setTenantId(multiStateInstanceUtil.getStateLevelTenant(tenantId));
 
-			if (tenantData != null) {
-				getDataForTenatId(mdmsCriteriaReq, tenantIdWithData, responseMap);
-			}
-		} else {
-			/*
-			 * if the tenantId contains separator, it will be backtracked until a tenant
-			 * with data is found
-			 */
-			for (int i = countOfSubTenant; i >= 0; i--) {
+		Map<String, String> schemaCodes = getSchemaCodes(mdmsCriteriaReq.getMdmsCriteria());
+		mdmsCriteriaReq.getMdmsCriteria().setSchemaCodeFilterMap(schemaCodes);
 
-				/*
-				 * pick new tenantId data only from the second loop
-				 */
-				if (i < countOfSubTenant)
-					tenantData = tenantIdMap.get(tenantIdWithData);
+		// Make a call to the repository layer to fetch data as per given criteria
+		tenantMasterMap = mdmsDataRepository.search(mdmsCriteriaReq.getMdmsCriteria());
 
-				/*
-					Get data and break, so that it doesn't split tenantId any further.
-					Check before splitting tenantId if it contains '.'
-				 */
-				if(tenantData != null) {
-					getDataForTenatId(mdmsCriteriaReq, tenantIdWithData, responseMap);
-					break;
-				} else if (tenantIdWithData.contains(".")) {
-					/*
-					 * trim the tenantId by "." separator to take the parent tenantId
-					 */
-					tenantIdWithData = tenantIdWithData.substring(0, tenantIdWithData.lastIndexOf("."));
+		// Apply filters to incoming data
+		tenantMasterMap = applyFilterToData(tenantMasterMap, mdmsCriteriaReq.getMdmsCriteria().getSchemaCodeFilterMap());
+
+		// Perform fallback
+		Map<String, JSONArray> masterDataMap = FallbackUtil.backTrackTenantMasterDataMap(tenantMasterMap, tenantId);
+
+		// Return response in MDMS v1 search response format for backward compatibility
+		return getModuleMasterMap(masterDataMap);
+	}
+
+	private Map<String, Map<String, JSONArray>> applyFilterToData(Map<String, Map<String, JSONArray>> tenantMasterMap, Map<String, String> schemaCodeFilterMap) {
+		Map<String, Map<String, JSONArray>> tenantMasterMapPostFiltering = new HashMap<>();
+
+		tenantMasterMap.keySet().forEach(tenantId -> {
+			Map<String, JSONArray> schemaCodeVsFilteredMasters = new HashMap<>();
+			tenantMasterMap.get(tenantId).keySet().forEach(schemaCode -> {
+				JSONArray masters = tenantMasterMap.get(tenantId).get(schemaCode);
+				if(!ObjectUtils.isEmpty(schemaCodeFilterMap.get(schemaCode))) {
+					schemaCodeVsFilteredMasters.put(schemaCode, filterMasters(masters, schemaCodeFilterMap.get(schemaCode)));
+				} else {
+					schemaCodeVsFilteredMasters.put(schemaCode, masters);
 				}
+			});
+			tenantMasterMapPostFiltering.put(tenantId, schemaCodeVsFilteredMasters);
+		});
+
+		return tenantMasterMapPostFiltering;
+	}
+
+	private JSONArray filterMasters(JSONArray masters, String filterExp) {
+		JSONArray filteredMasters = JsonPath.read(masters, filterExp);
+		return filteredMasters;
+	}
+
+	private Map<String, Map<String, JSONArray>> getModuleMasterMap(Map<String, JSONArray> masterMap) {
+		Map<String, Map<String, JSONArray>> moduleMasterMap = new HashMap<>();
+
+		for (Map.Entry<String, JSONArray> entry : masterMap.entrySet()) {
+			String[] moduleMaster = entry.getKey().split(DOT_REGEX);
+			String moduleName = moduleMaster[0];
+			String masterName = moduleMaster[1];
+
+			moduleMasterMap.computeIfAbsent(moduleName, k -> new HashMap<>())
+					.put(masterName, entry.getValue());
+		}
+		return moduleMasterMap;
+	}
+
+	private Map<String, String> getSchemaCodes(MdmsCriteria mdmsCriteria) {
+		Map<String, String> schemaCodesFilterMap = new HashMap<>();
+		for (ModuleDetail moduleDetail : mdmsCriteria.getModuleDetails()) {
+			for (MasterDetail masterDetail : moduleDetail.getMasterDetails()) {
+				String key = moduleDetail.getModuleName().concat(DOT_SEPARATOR).concat(masterDetail.getName());
+				String value = masterDetail.getFilter();
+				schemaCodesFilterMap.put(key, value);
 			}
 		}
-		return responseMap;
+		return schemaCodesFilterMap;
 	}
-
-	/**
-	 * method to filter module & master data from the given tenantId data
-	 * 
-	 * @param mdmsCriteriaReq
-	 * @param tenantId
-	 * @param responseMap
-	 */
-	public void getDataForTenatId(MdmsCriteriaReq mdmsCriteriaReq, String tenantId,
-			Map<String, Map<String, JSONArray>> responseMap) {
-
-		List<ModuleDetail> moduleDetails = mdmsCriteriaReq.getMdmsCriteria().getModuleDetails();
-        for (ModuleDetail moduleDetail : moduleDetails) {
-        	
-            List<MasterDetail> masterDetails = moduleDetail.getMasterDetails();
-            Map<String, JSONArray> finalMasterMap = new HashMap<>();
-
-            for (MasterDetail masterDetail : masterDetails) {
-            	
-				JSONArray masterData = null;
-				try {
-					masterData = getMasterDataFromTenantData(moduleDetail.getModuleName(), masterDetail.getName(),
-							tenantId);
-				} catch (Exception e) {
-					log.error("Exception occurred while reading master data", e);
-				}
-				
-				if (masterData == null)
-					continue;
-
-                if (masterDetail.getFilter() != null)
-                    masterData = filterMaster(masterData, masterDetail.getFilter());
-
-                finalMasterMap.put(masterDetail.getName(), masterData);
-            }
-            responseMap.put(moduleDetail.getModuleName(), finalMasterMap);
-        }
-	}
-
-	/**
-	 * Method to collect master data from module data
-	 * Automatically backtracks to parent tenant if data is not found for the master for the given tenantId
-	 * 
-	 * @param moduleName
-	 * @param masterName
-	 * @param tenantId
-	 * @return {@link JSONArray} jsonArray 
-	 * @throws Exception
-	 */
-	private JSONArray getMasterDataFromTenantData(String moduleName, String masterName, String tenantId)
-			throws Exception {
-
-		JSONArray jsonArray = null;
-		/*
-		 * local tenantId for backtracking parent tenant if data not available for given master
-		 */
-		String localTenantId = tenantId;
-		Map<String, Map<String, Map<String, JSONArray>>> tenantIdMap = MDMSApplicationRunnerImpl.getTenantMap();
-		Map<String, Map<String, JSONArray>> data;
-
-		int subTenatCount = StringUtils.countOccurrencesOf(tenantId, ".");
-
-		for (int i = subTenatCount; i >= 0; i--) {
-
-			data = tenantIdMap.get(localTenantId);
-			if (data != null && data.get(moduleName) != null && data.get(moduleName).get(masterName) != null) {
-				
-				jsonArray = data.get(moduleName).get(masterName);
-				/*
-				 * break and stop backtracking if data is found
-				 */
-				break;
-			} else {
-				/*
-				 * trim the tenantId by "." separator to take the parent tenantId
-				 */
-				if(localTenantId.contains("."))
-					localTenantId = localTenantId.substring(0, localTenantId.lastIndexOf("."));
-			}
-		}
-
-		log.info("ModuleName.... " + moduleName + " : MasterName.... " + masterName);
-		return jsonArray;
-	}
-
-	/*
-	 * Disabled isStateLevel tenant key 
-	 * and enabled backtracking for data true by default
-	 */
-	
-//	public Boolean isMasterBacktracingEnabled(String moduleName, String masterName) {
-//		Map<String, Map<String, Object>> masterConfigMap = MDMSApplicationRunnerImpl.getMasterConfigMap();
-//
-//		Map<String, Object> moduleData = masterConfigMap.get(moduleName);
-//		Boolean isStateLevel = false;
-//
-//		Object masterData = null;
-//		if (moduleData != null)
-//			masterData = moduleData.get(masterName);
-//
-//		if (null != masterData) {
-//			try {
-//				isStateLevel = JsonPath.read(mapper.writeValueAsString(masterData), MDMSConstants.STATE_LEVEL_JSONPATH);
-//			} catch (Exception e) {
-//				log.error("isStateLevelEnabled field missing default false value will be set");
-//			}
-//		}
-//		return isStateLevel;
-//	}
-
-    public JSONArray filterMaster(JSONArray masters, String filterExp) {
-        JSONArray filteredMasters = JsonPath.read(masters, filterExp);
-        return filteredMasters;
-    }
 }
